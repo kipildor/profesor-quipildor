ac249e419c305cdb2c8c15c665a4ab80826ce43a
kipildor <leandro_1980@live.com.ar>
2023-04-08 15:03:16 -0300

    Hasta TP 01 - EES 110

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,3 +4,10 @@ const mainMenuElement = document.getElementById('main-menu');
 toggleMenuElement.addEventListener('click', () => {
   mainMenuElement.classList.toggle('main-menu--show');
 });
+
+const dropdown = document.getElementById('dropdown');
+const dropdownMenu = document.getElementById('dropdown-menu');
+
+dropdown.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('dropdown-menu--show');
+});
